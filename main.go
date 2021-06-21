38d0030f168cdfd87afffaf52c3d1ac8ba5ee3bd
Yeonju Lee <jessy2yon@gmail.com>
2021-06-21 22:54:58 +0900

    request & check urls

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 var errRequestFailed = errors.New("Request failed")
 
 func main() {
+	// var results = map[string]string{}  // empty map 생성
+	var results = make(map[string]string)  // empty map 생성 (위와 동일)
+
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -17,15 +20,27 @@ func main() {
 		"https://www.google.com/",
 		"https://www.reddit.com/",
 	}
+	
 	for _, url := range urls {  // 첫번째 인자는 index, 두번째 인자는 value
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
+
 
 func hitURL(url string) error {
 	fmt.Println("Checking: ", url)
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
 		return errRequestFailed
 	}
 	return nil
